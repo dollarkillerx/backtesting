d04aba4e6cce7155f2ae46fa115e86d2ed4d88f1
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-08-27 18:02:59 +0900

    init project

--- rustdll/src/lib.rs
+++ rustdll/src/lib.rs
@@ -1,43 +1,26 @@
 mod conversion;
 use std::os::windows::ffi::OsStringExt;
+use serde::Serialize;
 use serde_json::json;
 use ureq;
-use crate::conversion::{double_ptr_to_vec, wide_ptr_to_string};
 
-#[no_mangle]
-pub extern "system" fn hello(count: i32) -> i32 {
-    if count < 0 {
-        return 0;
-    }
-    return 1;
+#[derive(Debug, Serialize, Clone)]
+struct LogData {
+    order_total: i64,
+    volume: f64,
+    profit: f64,
+    time: i64,
 }
 
+
 #[no_mangle]
-pub extern "system" fn fn_fill_array(arr: *mut f64, arr_size: i32) {
-   let resp = double_ptr_to_vec(arr, arr_size).unwrap();
-    let body: String = ureq::get(&format!("http://127.0.0.1:8181/{}", json!(resp).to_string()))
+pub extern "system" fn rs_log(order_total: i64, volume: f64, profit: f64, time: i64) {
+    let body: String = ureq::get(&format!("http://127.0.0.1:8181/{}", json!(LogData {order_total, volume, profit, time}).to_string()))
         .call()
         .unwrap()
         .into_string()
         .unwrap();
 
     println!("{}", body);
-}
-
-#[no_mangle]
-pub extern "system" fn hello2(text: *mut u16) {
-    if let Some(string) = wide_ptr_to_string(text) {
-        println!("Text: {}", string);
-
-        let body: String = ureq::get(&format!("http://127.0.0.1:8181/{}", string))
-            .call()
-            .unwrap()
-            .into_string()
-            .unwrap();
-
-        println!("{}", body);
-    } else {
-        println!("Received a null pointer");
-    }
 }
 
